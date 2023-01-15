67fd9f809583ccaab4f9803d6247fb3a66f6a044
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-15 11:36:51 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const submitBtn = document.getElementById("submit-btn");
+const searchBar = document.getElementById("search-bar");
 const audioPlayer = document.getElementById("audio-player");
 
 submitBtn.addEventListener("click", function(event) {
   event.preventDefault();
+  const searchQuery = searchBar.value;
+  const googleMapsUrl = `https://www.google.com/maps/search/${searchQuery}`;
   audioPlayer.play();
+  window.location = googleMapsUrl;
 });
